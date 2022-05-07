8c225fe239ad069953cfea291cc8360eb14de491
nitsuh21 <nitsuhdemissew21@gmail.com>
2022-05-07 04:17:50 -0700

    make it better

--- snp/settings.py
+++ snp/settings.py
@@ -141,7 +141,7 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Add these new lines
 
